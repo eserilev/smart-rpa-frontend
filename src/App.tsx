5c6f1bc82404c337005de23f9760f97592d20dc6
Eitan Seri-Levi <eserilev@ucsc.edu>
2021-04-06 15:09:33 -0700

    smart contract requests

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import smartRPA from './contracts/smartRPA';
 interface IProps {
 
 }
+
 interface IState {
   account?: string;
 }
@@ -16,21 +17,26 @@ class App extends Component<IProps, IState> {
   componentDidMount() {
     this.loadWeb3();
     this.loadBlockchainData();
-
-    console.log(JSON.stringify(smartRPA));
+    this.checkContract();
   }
 
   async loadWeb3() {
     if(window['ethereum'] ) {
       window['web3'] = new Web3(window['ethereum'])
       await window['ethereum'].enable();
-
-
+    }
+    else {
+      window['web3'] = new Web3(window['web3'].currentProvider)
     }
   }
 
+  async checkContract() {
+    let activeOffer = await smartRPA.methods.activeOffer().call();
+    console.log(activeOffer);
+  }
+
   async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const web3 = new Web3("http://localhost:8545");
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] })
   }
