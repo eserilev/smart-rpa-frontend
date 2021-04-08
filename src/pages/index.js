973f287372d9118f4f091576dc349f98c021a955
Sam Singer <sam.singet79@gmail.com>
2021-04-07 17:12:06 -0700

    Finished Home Page and Form Works Dates Restricted To The Future Only

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Navbar from "../components/Navbar";
 
 import Footer from "../components/Footer/index";
 import StepsToDo from "../components/StepsToDo";
+import SignInForm from "../components/ContractForm";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -13,13 +15,19 @@ const Home = () => {
   };
 
   return (
-    <>
+    <div className="Home">
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
-      <HeroSection />
-      <StepsToDo />
+      <Switch>
+        <Route exact path="/">
+          <HeroSection />
+          <StepsToDo />
+        </Route>
+        <Route exact path="/ContractForm" component={SignInForm}></Route>
+      </Switch>
+
       <Footer />
-    </>
+    </div>
   );
 };
 
